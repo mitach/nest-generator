1872ad515c87a82ead0833b43cd83efabfb5b02f
Dimitar Malinov <malinovd02@gmail.com>
2025-07-02 12:54:14 +0300

    asd

--- client/src/store/apiServiceStore.ts
+++ client/src/store/apiServiceStore.ts
@@ -6,8 +6,10 @@ interface ServiceStore {
   // other API clients can go here (authAPI, socketAPI, etc)
 }
 
+console.log('!!>> API BASE URL:', process.env.NEXT_PUBLIC_API_BASE_URL);
+
 export const useApiServiceStore = create<ServiceStore>(() => ({
   projectGeneratorAPI: new ProjectGeneratorAPI(
-    process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:5000',
+    process.env.NEXT_PUBLIC_API_BASE_URL || 'https://nest-generator-backend-560247957398.europe-west1.run.app',
   ),
 }));
